9492595784411c2304c99b7050d1fbdd2671d138
Miguel Gonzalez Mora <cuentapruebe@gmail.com>
2024-05-25 13:38:07 +0200

    skybox

---
+++ Unity/TFG/Assets/InvertNormals.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvertNormals : MonoBehaviour
+{
+    void Start()
+    {
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        if (meshFilter != null)
+        {
+            Mesh mesh = meshFilter.mesh;
+            Vector3[] normals = mesh.normals;
+            for (int i = 0; i < normals.Length; i++)
+            {
+                normals[i] = -normals[i];
+            }
+            mesh.normals = normals;
+
+            for (int i = 0; i < mesh.subMeshCount; i++)
+            {
+                int[] triangles = mesh.GetTriangles(i);
+                for (int j = 0; j < triangles.Length; j += 3)
+                {
+                    int temp = triangles[j];
+                    triangles[j] = triangles[j + 1];
+                    triangles[j + 1] = temp;
+                }
+                mesh.SetTriangles(triangles, i);
+            }
+        }
+    }
+}
